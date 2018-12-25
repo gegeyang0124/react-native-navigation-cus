c224a71e901112138bb2a2e39abb4c0b99fc42e1
zy <584450903@qq.com>
2018-12-25 17:28:45 +0800

    Signed-off-by: zy <584450903@qq.com>

--- tab/src/TabViewPagerPan.js
+++ tab/src/TabViewPagerPan.js
@@ -15,7 +15,7 @@ import type {
   TransitionConfigurator,
 } from './TabViewTypeDefinitions';
 
-import {StackPages} from "react-navigation";
+import {StackPages} from "./../../src/routers/StackPages";
 
 type GestureEvent = {
   nativeEvent: {
